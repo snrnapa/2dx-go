ac127266a5006026d0e047c45a51af33846d0dcc
snrnapa <shino.satoru@gmail.com>
2024-05-12 02:26:43 +0900

    moc apiを複数作成した

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # 2dx-go
+
+
+go mod init github.com/snrnapa/2dx-go
+
+go get github.com/gin-gonic/gin
--- main.go
+++ main.go
@@ -1,7 +1,50 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
 
 func main() {
-	fmt.Println("Hello 2dx")
+	router := gin.Default()
+
+	router.GET("login", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "completed user login",
+		})
+	})
+
+	router.GET("info", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "get basic ingo",
+		})
+	})
+
+	router.POST("info", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "crate new info",
+		})
+	})
+
+	router.DELETE("info", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "delete music data info",
+		})
+	})
+
+	router.PATCH("info", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "update part of  music data info",
+		})
+	})
+
+	router.GET("", func(c *gin.Context) {
+		c.String(http.StatusOK, "Hello 2DX")
+		fmt.Println("hello 2DX")
+
+	})
+
+	router.Run(":8080")
 }
